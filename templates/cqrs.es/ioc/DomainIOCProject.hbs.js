c06b8abdcd57ad056b8623b6c992f01af925e9e3
Dustin Lau <dlau@comsoftdev.com>
2020-04-16 17:09:34 -0500

    fixed samples

--- templates/cqrs.es/ioc/DomainIOCProject.hbs.js
+++ templates/cqrs.es/ioc/DomainIOCProject.hbs.js
@@ -3,6 +3,24 @@ module.exports = {
 
         //model.EventStorePort = 9991;
 
+
+        for (const domainEntity of model.DomainEntities) {
+
+            model.FirstAggregate = domainEntity.Name;
+
+            for (const view of domainEntity.Views) {
+                model.FirstView = view.Name;
+                break;
+            }
+
+            for (const action of domainEntity.Actions) {
+                model.FirstEvent = action.Event;
+                break;
+            }
+
+            break;    
+        }
+
         return model; 
     },
     prepareTarget: function(target) { return target; },
